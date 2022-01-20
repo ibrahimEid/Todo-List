ab7b44169a5b5e4e72a5ef1ee62148280a862950
ibrahimEid <ibr3idshn@gmail.com>
2022-01-20 11:56:51 +0300

    get and post version 1

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,30 @@ const app = express()
 const db = require('./db')
 const Todo = require('./todo')
 // console.log(Todo)
-
+app.use(express.json())
 app.get("/", (req,res)=>{
     res.json('Get / is working')
 })
 
 app.get("/todos", (req,res)=>{
-    res.json('Get / is working')
+    Todo.find({}, (err, data)=> {
+        if(err){
+            console.log(`Error: ${err}`)
+        }else{
+            res.send(data)
+        }  
+      })
+})
+
+app.post("/todos",(req,res)=>{
+    // console.log(req.body)
+    Todo.create(req.body, (err, newTask)=>{
+        if(err){
+            console.log(`Error: ${err}`)  
+        }else{
+          res.status(201).json(newTask)  
+        }
+    })
 })
 
 app.listen(5000,()=>{
