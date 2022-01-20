4db47b0a81dec7301bf3aa564602558d197387ec
ibrahimEid <ibr3idshn@gmail.com>
2022-01-20 14:43:10 +0300

    put and delete router version 1

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.get("/", (req,res)=>{
     res.json('Get / is working')
 })
 
-app.get("/todos", (req,res)=>{
+app.get("/todos/", (req,res)=>{
     Todo.find({}, (err, data)=> {
         if(err){
             console.log(`Error: ${err}`)
@@ -19,13 +19,43 @@ app.get("/todos", (req,res)=>{
       })
 })
 
-app.post("/todos",(req,res)=>{
+app.post("/todos/",(req,res)=>{
     // console.log(req.body)
     Todo.create(req.body, (err, newTask)=>{
         if(err){
             console.log(`Error: ${err}`)  
         }else{
           res.status(201).json(newTask)  
+        }
+    })
+})
+
+app.put("/todos/:id", (req,res)=>{
+    Todo.updateOne(
+        {_id: req.params.id},
+        {title:req.body.title},
+        (err,updateObj)=>{
+            if(err){
+                console.log(`Error: ${err}`)
+                res.status(400).json(err)
+            }else{
+                console.log(updateObj)
+                updateObj.matchedCount === 1
+                ?res.json('update this todo')
+                :res.status(404).json("This todo is not found")
+            }
+        }
+    )
+})
+
+app.delete("/todos/:id", (req,res)=>{
+    Todo.deleteOne({_id:req.params.id},(err, deleteObj)=>{
+        if(err){
+            console.log(`Error: ${err}`)
+        }else{
+            deleteObj.deletedCount === 1
+            ?res.json('Delete this todo')
+            :res.status(404).json("This todo is not found")
         }
     })
 })
